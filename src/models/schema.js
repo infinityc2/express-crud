a1971a880fc42d7192377cc7f1c9e36782cd4d77
infinityc2 <super_star@hotmail.co.th>
2019-11-03 03:39:21 +0700

    database relationship

--- src/models/schema.js
+++ src/models/schema.js
@@ -2,12 +2,17 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const authorSchema = new Schema({
+    name: { type: String, required: true },
+    phone: { type: String, required: true}
+}, { timestamps: true, versionKey: false })
+const authorModel = mongoose.model("Authors", authorSchema)
+
 const bookSchema = new Schema({
     name: { type: String, required: true },
     describe: { type: String, required: true },
-    author: { type: String, required: true, },
+    author: { type: authorSchema },
 }, { timestamps: true, versionKey: false })
-
 const bookModel = mongoose.model("Books", bookSchema)
 
-module.exports = bookModel
+module.exports = { bookModel, authorModel }
--- src/router/router.js
+++ src/router/router.js
@@ -1,22 +1,24 @@
 import express from 'express'
-import Books from '../models/schema'
+import { bookModel, authorModel } from '../models/schema'
 
 const router = express.Router()
 
 router.get('/books', async (req, res) => {
-    const books = await Books.find()
+    const books = await bookModel.find()
     res.json(books)
 })
 
 router.get('/books/:id', async (req, res) => {
     const { id } = req.params
-    const book = await Books.findById(id)
+    const book = await bookModel.findById(id)
     res.json(book)
 })
 
 router.post('/books', async (req, res) => {
     const payload = req.body
-    const book = new Books(payload)
+    const author = new authorModel({ name: payload.author.name, phone: payload.author.phone})
+    await author.save()
+    const book = new bookModel({ name: payload.name, describe: payload.describe, author: author })
     await book.save()
     res.status(201).end()
 })
@@ -24,13 +26,44 @@ router.post('/books', async (req, res) => {
 router.put('/books/:id', async (req, res) => {
     const { id } = req.params
     const payload = req.body
-    const book = await Books.findByIdAndUpdate(id, { $set: payload })
+    const book = await bookModel.findByIdAndUpdate(id, { $set: payload })
     res.json(book)
 })
 
 router.delete('/books/:id', async (req, res) => {
     const { id } = req.params
-    await Books.findByIdAndDelete(id)
+    await bookModel.findByIdAndDelete(id)
+    res.status(204).end()
+})
+
+router.get('/authors', async (req, res) => {
+    const authors = await authorModel.find()
+    res.json(authors)
+})
+
+router.get('/authors/:id', async (req, res) => {
+    const { id } = req.params
+    const author = await authorModel.findById(id)
+    res.json(author)
+})
+
+router.post('/authors', async (req, res) => {
+    const payload = req.body
+    const author = new authorModel(payload)
+    await author.save()
+    res.status(201).end()
+})
+
+router.put('/authors/:id', async (req, res) => {
+    const { id } = req.params
+    const payload = req.body
+    const author = await authorModel.findByIdAndUpdate(id, { $set: payload })
+    res.json(author)
+})
+
+router.delete('/authors', async (req, res) => {
+    const { id } = req.params
+    await authorModel.findByIdAndDelete(id)
     res.status(204).end()
 })
 
